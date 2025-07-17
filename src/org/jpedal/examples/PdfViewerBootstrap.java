596559bb929d8ee7ffa495e17a6913b587769d6d
huanggp@yunjiacloud.com <huanggp@yunjiacloud.com>
2025-07-17 18:41:34 +0800

    feat init

---
+++ src/org/jpedal/examples/PdfViewerBootstrap.java
@@ -0,0 +1,10 @@
+package org.jpedal.examples;
+
+
+import org.jpedal.examples.viewer.FXStartup;
+
+public class PdfViewerBootstrap {
+    public static void main(String[] args) {
+        FXStartup.main(args);
+    }
+}
---
+++ src/org/jpedal/examples/viewer/commands/javafx/JavaFXTipOfTheDay.java
@@ -0,0 +1,48 @@
+/*
+ * ===========================================
+ * Java Pdf Extraction Decoding Access Library
+ * ===========================================
+ *
+ * Project Info:  http://www.idrsolutions.com
+ * Help section for developers at http://www.idrsolutions.com/support/
+ *
+ * (C) Copyright 1997-2015 IDRsolutions and Contributors.
+ *
+ * This file is part of JPedal/JPDF2HTML5
+ *
+     This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+ *
+ * ---------------
+ * JavaFXTipOfTheDay.java
+ * ---------------
+ */
+
+package org.jpedal.examples.viewer.commands.javafx;
+
+import org.jpedal.examples.viewer.utils.PropertiesFile;
+import org.jpedal.gui.GUIFactory;
+
+/**
+ * Class Which Displays the Tip of the Day
+ * Can be toggle to display on Viewer Startup.
+ */
+public class JavaFXTipOfTheDay {
+    
+    public static void execute(final Object[] args, final GUIFactory currentGUI, final PropertiesFile properties) {
+
+    }
+}
